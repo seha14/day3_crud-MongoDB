e5ca810eae1eeb6fb04cbdf4895a7eb0e38abdff
seha14 <sehabuddinnasution@gmail.com>
2019-07-29 09:40:37 +0700

    day3_crud-MongoDB

---
+++ penjualan.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.12 
+MySQL - 5.5.5-10.1.25-MariaDB : Database - penjualan
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`penjualan` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `penjualan`;
+
+/*Table structure for table `barang` */
+
+DROP TABLE IF EXISTS `barang`;
+
+CREATE TABLE `barang` (
+  `kd_brg` char(6) NOT NULL,
+  `nm_brg` varchar(30) NOT NULL,
+  `satuan` varchar(10) DEFAULT NULL,
+  `harga` double DEFAULT NULL,
+  `harga_beli` double DEFAULT NULL,
+  `stok` int(5) DEFAULT NULL,
+  `stok_min` int(5) DEFAULT NULL,
+  PRIMARY KEY (`kd_brg`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `barang` */
+
+insert  into `barang`(`kd_brg`,`nm_brg`,`satuan`,`harga`,`harga_beli`,`stok`,`stok_min`) values ('aaaa','assasaas','kg',10000,15000,3,3),('B-0002','sayur pecel','buah',5000,4000,100,10),('B-0003','Embohhh','3',5000,4500,5,4);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
